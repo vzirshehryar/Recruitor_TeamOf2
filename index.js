e539251b186a8308cb3c7ce6f94ed80f9d961139
vzirshehryar <vzirshehryar@gmail.com>
2023-07-27 20:16:45 +0500

    internal server error fixed

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ import path from "path";
 import UserRoutes from "./Server/router/users.js";
 import AdminRoutes from "./Server/router/admin.js";
 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
