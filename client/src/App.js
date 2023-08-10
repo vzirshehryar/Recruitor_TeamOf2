08b9af613b77e016621111f6211f8667ce9e9dbf
vzirshehryar <vzirshehryar@gmail.com>
2023-08-10 23:59:44 +0500

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -44,57 +44,6 @@ import SubmitSkills from "./routes/Profile/components/Skills/SubmitSkills";
 import Sidebar from "./routes/company/Sidebar";
 import Jobs from "./routes/company/Dashboard/PostJobs/Jobs";
 import Profile from "./routes/company/Dashboard/CompProfile/Profile";
-
-import CompanySignUp from "./routes/Auth/signUp/CompanySignUp";
-import LoginAsCompany from "./routes/Auth/Login/LoginAsCompany";
-import CompanyHome from "./routes/company/home/Home";
-
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Home from "./routes/Home/index";
-import Login from "./routes/Auth/Login/index";
-import ProtectedRoute from "./components/ProtectedRoute";
-import SignUp from "./routes/Auth/signUp/index";
-import ForgetPassword from "./routes/Auth/ForgetPassword/index";
-import SetPassword from "./routes/Auth/setPassword/index";
-import JobFeed from "./routes/jobFeed/JobFeed";
-import PersonalInfo from "./routes/Profile/components/personalinfo";
-import Sidenav from "./routes/Profile/components/Sidenav";
-import Images from "./routes/Profile/components/Images";
-import PersonalInformation from "./routes/Profile/components/Information";
-import AddExperiences from "./routes/Profile/components/AddExperiences";
-import Experiences from "./routes/Profile/components/Experiences/Experiences";
-import AddEducation from "./routes/Profile/components/Education/AddEducation";
-import Education from "./routes/Profile/components/Education/Education";
-import AddCourses from "./routes/Profile/components/Courses/AddCourses";
-import Courses from "./routes/Profile/components/Courses/Courses";
-import AddCertifications from "./routes/Profile/components/Certifications/AddCertifications";
-import Certifications from "./routes/Profile/components/Certifications/Certifications";
-import AddAwards from "./routes/Profile/components/Awards/AddAwards";
-import Awards from "./routes/Profile/components/Awards/Awards";
-import AddPublications from "./routes/Profile/components/Publications/AddPublications";
-import Publications from "./routes/Profile/components/Publications/Publications";
-import AddLanguages from "./routes/Profile/components/Languages/AddLanguages";
-import Languages from "./routes/Profile/components/Languages/Languages";
-import AddSkills from "./routes/Profile/components/Skills/AddSkills";
-import SubmitExp from "./routes/Profile/components/Experiences/SubmitExp";
-import Skills from "./routes/Profile/components/Skills/Skills";
-import SubmitEdu from "./routes/Profile/components/Education/SubmitEdu";
-import SubmitCour from "./routes/Profile/components/Courses/SubmitCour";
-import SubmitCert from "./routes/Profile/components/Certifications/SubmitCert";
-import SubmitAwards from "./routes/Profile/components/Awards/SubmitAwards";
-import SubmitPub from "./routes/Profile/components/Publications/SubmitPub";
-import SubmitLang from "./routes/Profile/components/Languages/SubmitLang";
-import SubmitSkills from "./routes/Profile/components/Skills/SubmitSkills";
-import Sidebar from "./routes/company/Sidebar";
-import Jobs from "./routes/company/Dashboard/PostJobs/Jobs";
-import Profile from "./routes/company/Dashboard/CompProfile/Profile";
 import CompanySignUp from "./routes/Auth/signUp/CompanySignUp";
 import LoginAsCompany from "./routes/Auth/Login/LoginAsCompany";
 import Applicants from "./routes/company/Dashboard/AllApplicants/Applicants";
