0ad941ad6160f98eca3a12089c9a0c8a61ce7036
Elee Lawleit <alibutt200235@gmail.com>
2023-08-28 11:25:37 +0500

    landing page cards

--- client/src/routes/company/home/Home.jsx
+++ client/src/routes/company/home/Home.jsx
@@ -33,6 +33,52 @@ const Home = () => {
           <img src={guyWithMic} alt="" />
         </div>
       </div>
+      <Container id="whyus" className="py-5 p-3 no-mwidth">
+        <h1
+          className="text-center whyUsMainHeading"
+        >
+          Our Approach
+        </h1>
+        <br></br>
+        <Container className="whyUsSecondHeadingCont">
+          <h2
+            className="text-center whyUsSecondHeading"
+            style={{ color: "#404145", maxWidth: "900px" }}
+          >
+            Experience the difference with our unparalleled recruitment
+            solutions that connect talent with opportunity.
+          </h2>
+        </Container>
+        <Container className="pt-5 pb-5 no-mwidth">
+          <div className="d-flex justify-content-between flex-wrap text-center p-3 gap-3">
+            <div className="whyUsContentCont">
+              <h1 className="whyUsContHeader pt-2">Understand who you are</h1>
+              <p className="whyUsContText pt-2">
+                You tell us what your DNA is and we will match you with the
+                right candidate.
+              </p>
+            </div>
+            <div className="whyUsContentCont">
+              <h1 className="whyUsContHeader pt-2">Identify your needs</h1>
+              <p className="whyUsContText pt-2">
+                What skills, but above all what qualities will make it possible
+                to establish a harmonious and positive working environment
+                between you and your future employee?
+              </p>
+            </div>
+            <div className="whyUsContentCont">
+              <h1 className="whyUsContHeader pt-2">
+                Select the right candidates
+              </h1>
+              <p className="whyUsContText pt-2">
+                Deside Recrutement favours quality over quantity. We will
+                present you with the best and the most relevant candidates to
+                match your requirement
+              </p>
+            </div>
+          </div>
+        </Container>
+      </Container>
     </div>
   );
 };
