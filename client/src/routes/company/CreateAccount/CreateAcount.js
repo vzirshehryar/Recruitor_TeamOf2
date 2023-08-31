51b2fbb58956771e33a1bbacdb1e110dff5772e1
moeezramay <moeezramay1@gmail.com>
2023-08-31 22:03:44 +0500

    company page almost completed

--- client/src/routes/company/CreateAccount/CreateAcount.js
+++ client/src/routes/company/CreateAccount/CreateAcount.js
@@ -7,37 +7,37 @@ import { useContext } from "react";
 import axios from "axios";
 
 export const CreateAcount = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const { companyInfo, setcompanyInfo } = useContext(UserContext);
+    const { companyInfo, setcompanyInfo } = useContext(UserContext);
 
-  async function NextPage(e) {
-    e.preventDefault();
-    console.log("Running");
-    try {
-      const response = await fetch("/company/setProfile", {
-        method: "POST",
-        headers: {
-          Authorization: `${localStorage.getItem("token")}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(companyInfo),
-      });
-      console.log("response: ", response);
-      navigate("/company/jobbasics");
-    } catch (e) {
-      console.log(e);
+    async function NextPage(e) {
+        e.preventDefault();
+        console.log("Running");
+        try {
+            const response = await fetch("/company/setProfile", {
+                method: "POST",
+                headers: {
+                    Authorization: `${localStorage.getItem("token")}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(companyInfo),
+            });
+            console.log("response: ", response);
+            navigate("/company/jobbasics");
+        } catch (e) {
+            console.log(e);
+        }
     }
-  }
 
-  const handleInputChange = (e) => {
-    setcompanyInfo({ ...companyInfo, [e.target.id]: e.target.value });
-    console.log(companyInfo);
-  };
+    const handleInputChange = (e) => {
+        setcompanyInfo({ ...companyInfo, [e.target.id]: e.target.value });
+        console.log(companyInfo);
+    };
 
-  return (
-    <div className="create-account-main-container">
-      {/* <div className="horizontal-timeline">
+    return (
+        <div className="create-account-main-container">
+            {/* <div className="horizontal-timeline">
         <ul className="horizontal-timeline-list">
           <li className="li1">1</li>
           <li className="li2">2</li>
@@ -46,69 +46,77 @@ export const CreateAcount = () => {
           <li className="li5">5</li>
         </ul>
       </div> */}
-      <div className="create-account-heading-container">
-        <h1 className="create-account-heading">Create an employer account</h1>
-      </div>
-      <div className="create-account-para-container">
-        <p className="create-account-para">
-          You haven't posted a job before, so you'll need to create an employer
-          account.
-        </p>
-      </div>
-      <div className="create-account-form-container">
-        <form action="" onSubmit={NextPage}>
-          <div>
-            <label htmlFor="">Your company's name</label>
-            <input
-              type="text"
-              name=""
-              id="name"
-              placeholder="XYZ"
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="">Your company's number of employees</label>
-            <input
-              type="text"
-              name=""
-              id="teamSize"
-              placeholder="10-1000"
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="">Your first and last name</label>
-            <input
-              type="text"
-              name=""
-              id="firstAndLastName"
-              placeholder="Name"
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="">
-              Your phone number <br />
-              (For account management communication. Not visible to jobseekers.)
-            </label>
-            <input
-              type="text"
-              name=""
-              id="phNo"
-              placeholder="00000000000"
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-          <button type="submit" className="create-account-submit-button">
-            Continue
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+            <div className="create-account-heading-container">
+                <h1 className="create-account-heading">
+                    Create an employer account
+                </h1>
+            </div>
+            <div className="create-account-para-container">
+                <p className="create-account-para">
+                    You haven't posted a job before, so you'll need to create an
+                    employer account.
+                </p>
+            </div>
+            <div className="create-account-form-container">
+                <form action="" onSubmit={NextPage}>
+                    <div>
+                        <label htmlFor="">Your company's name</label>
+                        <input
+                            type="text"
+                            name=""
+                            id="name"
+                            placeholder="XYZ"
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">
+                            Your company's number of employees
+                        </label>
+                        <input
+                            type="text"
+                            name=""
+                            id="teamSize"
+                            placeholder="10-1000"
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">Your first and last name</label>
+                        <input
+                            type="text"
+                            name=""
+                            id="firstAndLastName"
+                            placeholder="Name"
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">
+                            Your phone number <br />
+                            (For account management communication. Not visible
+                            to jobseekers.)
+                        </label>
+                        <input
+                            type="text"
+                            name=""
+                            id="phNo"
+                            placeholder="00000000000"
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="create-account-submit-button"
+                    >
+                        Continue
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
--- client/src/routes/company/JobBasics/JobBasics.js
+++ client/src/routes/company/JobBasics/JobBasics.js
@@ -6,24 +6,23 @@ import { UserContext } from "../companyContext";
 import { useContext } from "react";
 
 export const JobBasics = () => {
-    const { techDetails, setTechDetails } = useContext(UserContext);
-    console.log(techDetails);
+    const { jobBasics, setjobBasics } = useContext(UserContext);
+    console.log(jobBasics);
 
     const handleInputChange = (e) => {
-        setTechDetails({ ...techDetails, [e.target.id]: e.target.value });
-        console.log(techDetails);
+        setjobBasics({ ...jobBasics, [e.target.id]: e.target.value });
+        console.log(jobBasics);
     };
 
     const navigate = useNavigate();
 
-    function NextPage(e) {
+    async function NextPage(e) {
         e.preventDefault();
-        console.log("Running");
         navigate("/company/techdetail");
     }
     return (
-      <div className="create-account-main-container">
-        {/* <div className="horizontal-timeline">
+        <div className="create-account-main-container">
+            {/* <div className="horizontal-timeline">
         <ul className="horizontal-timeline-list">
           <li className="li1">1</li>
           <li className="li2">2</li>
@@ -32,66 +31,69 @@ export const JobBasics = () => {
           <li className="li5">5</li>
         </ul>
       </div> */}
-        <div className="create-account-heading-container">
-          <h1 className="create-account-heading">Add job Basics</h1>
+            <div className="create-account-heading-container">
+                <h1 className="create-account-heading">Add job Basics</h1>
+            </div>
+            <div className="create-account-form-container">
+                <form action="" onSubmit={NextPage}>
+                    <div>
+                        <label htmlFor="">Company&apos;s Industry</label>
+                        <input
+                            type="text"
+                            name=""
+                            id="industry"
+                            placeholder="industry"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">Job Title</label>
+                        <input
+                            type="text"
+                            name=""
+                            id="jobTitle"
+                            placeholder="Desginer"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">Job Type</label>
+                        <input
+                            type="text"
+                            name=""
+                            id="jobType"
+                            placeholder="Hybrd"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">Location</label>
+                        <input
+                            type="text"
+                            name=""
+                            id="location"
+                            placeholder="address"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">Company Description</label>
+                        <textarea
+                            rows={7}
+                            type="text"
+                            name=""
+                            id="jobDescription"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="create-account-submit-button"
+                    >
+                        Continue
+                    </button>
+                </form>
+            </div>
         </div>
-        <div className="create-account-form-container">
-          <form action="" onSubmit={NextPage}>
-            <div>
-              <label htmlFor="">Company&apos;s Industry</label>
-              <input
-                type="text"
-                name=""
-                id="industry"
-                placeholder="industry"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="">Job Title</label>
-              <input
-                type="text"
-                name=""
-                id="jobTitle"
-                placeholder="Desginer"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="">Job Type</label>
-              <input
-                type="text"
-                name=""
-                id="jobType"
-                placeholder="Hybrd"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="">Location</label>
-              <input
-                type="text"
-                name=""
-                id="location"
-                placeholder="address"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="">Company Description</label>
-              <textarea
-                rows={7}
-                type="text"
-                name=""
-                id="jobDescription"
-                onChange={handleInputChange}
-              />
-            </div>
-            <button type="submit" className="create-account-submit-button">
-              Continue
-            </button>
-          </form>
-        </div>
-      </div>
     );
 };
