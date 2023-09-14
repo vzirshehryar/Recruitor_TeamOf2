05fbc4a8b9a4fcf4743c4e492e442a9ea7f971ec
moeezramay <moeezramay1@gmail.com>
2023-09-14 17:59:52 +0500

    Detail page fixed

--- Server/controllers/userProfileControllers/jobExperience.js
+++ Server/controllers/userProfileControllers/jobExperience.js
@@ -2,62 +2,63 @@ import JobExperience from "../../models/userModels/JobExperience.js";
 import { User } from "../../models/User.js";
 
 export const postData = async (req, res) => {
-  try {
-    const data = req.body;
-    const userID = req.user; // it is set from middleware
-    var progress = 0;
-    const checkForProgress = await JobExperience.find({ user: userID });
-    if (checkForProgress.length === 0) {
-      const user = await User.findById(userID);
-      user.profileCompletion += 10;
-      progress = 10;
-      await user.save();
-    }
-    console.log(data);
-    if (
-      !data.jobTitle ||
-      !data.employmentType ||
-      !data.industry ||
-      !data.startDate ||
-      !data.company ||
-      !data.companyWebsite ||
-      !data.country ||
-      !data.state ||
-      !data.city ||
-      !data.description
-    ) {
-      res.status(400).json({ error: "data in body is not complete" });
-      return;
-    }
+    try {
+        const data = req.body;
+        const userID = req.user; // it is set from middleware
+        var progress = 0;
+        const checkForProgress = await JobExperience.find({ user: userID });
+        if (checkForProgress.length === 0) {
+            const user = await User.findById(userID);
+            user.profileCompletion += 10;
+            progress = 10;
+            await user.save();
+        }
+        console.log(data);
+        if (
+            !data.jobTitle ||
+            !data.employmentType ||
+            !data.industry ||
+            !data.startDate ||
+            !data.company ||
+            !data.companyWebsite ||
+            !data.country ||
+            !data.state ||
+            !data.city ||
+            !data.description
+        ) {
+            res.status(400).json({ error: "data in body is not complete" });
+            return;
+        }
 
-    data.user = userID;
-    const jobExperience = new JobExperience(data);
-    const job = await jobExperience.save();
+        data.user = userID;
+        const jobExperience = new JobExperience(data);
+        const job = await jobExperience.save();
 
-    return res.status(201).json({
-      progress: progress,
-      message: "Job Experience Added Successfully",
-    });
-  } catch (error) {
-    console.error("Error creating user:", error);
-    return res.status(400).json({
-      error: "An error occurred while creating Job Experience",
-    });
-  }
+        return res.status(201).json({
+            progress: progress,
+            message: "Job Experience Added Successfully",
+        });
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(400).json({
+            error: "An error occurred while creating Job Experience",
+        });
+    }
 };
 
 export const getData = async (req, res) => {
-  try {
-    const userID = req.user; // it is set from middleware
-    const jobExperience = await JobExperience.find({ user: userID });
+    console.log("get data called");
+    try {
+        const userID = req.user; // it is set from middleware
+        const jobExperience = await JobExperience.find({ user: userID });
 
-    return res.status(201).json({
-      jobs: jobExperience,
-    });
-  } catch (error) {
-    console.error("Error creating user:", error);
-    return res.status(400).json({
-      error: "An error occurred while creating Job Experience",
-    });
-  }
+        return res.status(201).json({
+            jobs: jobExperience,
+        });
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(400).json({
+            error: "An error occurred while creating Job Experience",
+        });
+    }
 };
--- client/src/routes/jobFeed/job/DetailPage.js
+++ client/src/routes/jobFeed/job/DetailPage.js
@@ -15,112 +15,129 @@ import { Container, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 function calculateDaysSincePosted(dateposted) {
-  const currentDate = Date.now();
+    const currentDate = Date.now();
 
-  // Calculate the time difference in milliseconds
-  const timeDifference = currentDate - dateposted;
+    // Calculate the time difference in milliseconds
+    const timeDifference = currentDate - dateposted;
 
-  console.log(timeDifference);
-  // Calculate the number of days
-  const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    console.log(timeDifference);
+    // Calculate the number of days
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-  return daysDifference;
+    return daysDifference;
 }
 
 function DetailPage() {
-  const [jobData, setJobData] = useState({});
-  const { jobID } = useParams(); // Extract jobId from URL params
-  const apiEndpoint = `http://localhost:4000/job/getJob/${jobID}`;
+    const [jobData, setJobData] = useState({});
+    const { jobID } = useParams(); // Extract jobId from URL params
+    const apiEndpoint = `http://localhost:4000/job/getJob/${jobID}`;
 
-  useEffect(() => {
-    fetch(apiEndpoint)
-      .then((response) => response.json())
-      .then((data) => {
-        setJobData(data.job);
-        console.log(data.job);
-      })
-      .catch((error) => {
-        console.error("Error fetching job data:", error);
-      });
-  }, [apiEndpoint]);
+    useEffect(() => {
+        fetch(apiEndpoint)
+            .then((response) => response.json())
+            .then((data) => {
+                setJobData(data.job);
+                console.log(data.job);
+            })
+            .catch((error) => {
+                console.error("Error fetching job data:", error);
+            });
+    }, [apiEndpoint]);
 
-  return (
-    <>
-      <Header />
-      <JobNav />
-      <div className=" d-flex justify-content-center p-3 ">
-        <div className={`container dpage m-0    Card col-lg-3  `}>
-          <div className="position-relative d-flex flex-column align-items-center">
-            <div
-              className=" bgimg"
-              style={{ marginBottom: "120px", width: "100%" }}
-            >
-              <div className="div-box-main-color">
-                
-              <div className="div-box-main-color-heading">
-              Job Title
-            </div>
-          </div>
-            </div>
-            {/* TITLE PART AND SMALL INFORMATIONS*/}
-            <div>
-              <div className="ml-5 dp d-flex justify-content-start ">
-                <p className="dheading   ">Featured</p>
-              </div>
-              <div>
-                <Container className="Card  dcard ">
-                  <Row>
-                    <Col className=" " md={{ span: 6, offset: 4 }}>
-                      <div>
-                        <h1 className="dhead  m-0 p-0">{jobData.jobTitle}</h1>
-                        <p className="phead   m-0 p-0">
-                          Posted {calculateDaysSincePosted(jobData.postedOn)}{" "}
-                          days ago
-                        </p>
-                      </div>
-                    </Col>
-                    {/* <Col>
+    return (
+        <>
+            <Header />
+            <JobNav />
+            <div className=" d-flex justify-content-center p-3 ">
+                <div className={`container dpage m-0    Card col-lg-3  `}>
+                    <div className="position-relative d-flex flex-column align-items-center">
+                        <div
+                            className=" bgimg"
+                            style={{ marginBottom: "120px", width: "100%" }}
+                        >
+                            <div className="div-box-main-color">
+                                <div className="div-box-main-color-heading">
+                                    {jobData.industry}
+                                </div>
+                            </div>
+                        </div>
+                        {/* TITLE PART AND SMALL INFORMATIONS*/}
+                        <div>
+                            <div className="ml-5 dp d-flex justify-content-start ">
+                                <p className="dheading   ">Featured</p>
+                            </div>
+                            <div>
+                                <Container className="Card  dcard ">
+                                    <Row>
+                                        <Col
+                                            className=" "
+                                            md={{ span: 6, offset: 4 }}
+                                        >
+                                            <div>
+                                                <h1 className="dhead  m-0 p-0">
+                                                    {jobData.jobTitle}
+                                                </h1>
+                                                <p className="phead   m-0 p-0">
+                                                    Posted{" "}
+                                                    {calculateDaysSincePosted(
+                                                        jobData.postedOn
+                                                    )}{" "}
+                                                    days ago
+                                                </p>
+                                            </div>
+                                        </Col>
+                                        {/* <Col>
                       <div className="  gap-3 " md={{ span: 3, offset: 3 }}>
                         <AiOutlineLike className="" />
                         <BsShare />
                       </div>
                     </Col> */}
-                  </Row>
-                  <Row>
-                    <Col></Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <div className="dloc ">
-                        <div className="d-flex  grid text-center gap-4  justify-content-center ">
-                          <p className="dl p-2">
-                            {jobData.maxSR ? "$ " + jobData.maxSR : "$ 30"} per
-                            hours
-                          </p>
-                          <p className="dl p-2 ">{jobData.jobType}</p>
-                          <p className=" p-2 dl ">{jobData.location}</p>
+                                    </Row>
+                                    <Row>
+                                        <Col></Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <div className="dloc ">
+                                                <div className="d-flex  grid text-center gap-4  justify-content-center ">
+                                                    <p className="dl p-2">
+                                                        {jobData.maxSR
+                                                            ? "$ " +
+                                                              jobData.maxSR
+                                                            : "$ 30"}{" "}
+                                                        per hours
+                                                    </p>
+                                                    <p className="dl p-2 ">
+                                                        {jobData.jobType}
+                                                    </p>
+                                                    <p className=" p-2 dl ">
+                                                        {jobData.location}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </div>
                         </div>
-                      </div>
-                    </Col>
-                  </Row>
-                </Container>
-              </div>
-            </div>
-            {/* Overview, Background/Requirements, Required Skills, Application Questions */}
-            <div className="d-flex justify-content-center px-5">
-              <div className="dpad">
-                <h2 className="dheading mt-5 "> Job Description</h2>
-                <p className="phead mt-4 px-3">
-                  {jobData.jobDescription
-                    ? jobData.jobDescription
-                    : `I am currently working with a warm and friendly company, who
+                        {/* Overview, Background/Requirements, Required Skills, Application Questions */}
+                        <div className="d-flex justify-content-center px-5">
+                            <div className="dpad">
+                                <h2 className="dheading mt-5 ">
+                                    {" "}
+                                    Job Description
+                                </h2>
+                                <p className="phead mt-4 px-3">
+                                    {jobData.jobDescription
+                                        ? jobData.jobDescription
+                                        : `I am currently working with a warm and friendly company, who
                   due to increased work are now seeking an additional Graphic
                   Designer to bring fresh ideas and concepts to fit both Print
                   and Digital briefs.Based at company’s Head Office in Oldbury
                   this is initially a 3-month full time contract, however there
                   is the possibility it could develop into a permanent position`}
-                </p>
-                {/* <h2 className="dheading dpad ">Overview:</h2>
+                                </p>
+                                {/* <h2 className="dheading dpad ">Overview:</h2>
                 <ul className="dli lh-lg  mt-4 px-3">
                   <li className="">
                     Working closely with the In-House Team and Directors as well
@@ -167,27 +184,31 @@ function DetailPage() {
                     Ability to communicate your creative vision across the team.
                   </li>
                 </ul> */}
-                <h2 className="dheading dpad">Required skills</h2>
-                <div className="mt-4 px-5 d-flex gap-3  text-center flex-wrap">
-                  {jobData.skills &&
-                    jobData.skills.map((val, ind) => {
-                      return (
-                        <h4
-                          style={{ background: "#D3D3D3" }}
-                          className="dl text-center px-2 py-1 border rounded"
-                          key={ind}
-                        >
-                          {val.skill}
-                        </h4>
-                      );
-                    })}
-                  {/* <p className="dl text-center p-3   ">Adobe</p>
+                                <h2 className="dheading dpad">
+                                    Required skills
+                                </h2>
+                                <div className="mt-4 px-5 d-flex gap-3  text-center flex-wrap">
+                                    {jobData.skills &&
+                                        jobData.skills.map((val, ind) => {
+                                            return (
+                                                <h4
+                                                    style={{
+                                                        background: "#D3D3D3",
+                                                    }}
+                                                    className="dl text-center px-2 py-1 border rounded"
+                                                    key={ind}
+                                                >
+                                                    {val.skill}
+                                                </h4>
+                                            );
+                                        })}
+                                    {/* <p className="dl text-center p-3   ">Adobe</p>
                   <h4 className="dl text-center p-3">Creative</h4>
                   <h4 className="dl text-center p-3">Website</h4>
                   <h4 className="dl text-center p-3">Mac</h4>
                   <h4 className="dl text-center p-3">Digital Designs</h4> */}
-                </div>
-                {/* <h1 className="dheading dpad"> Application questions</h1>
+                                </div>
+                                {/* <h1 className="dheading dpad"> Application questions</h1>
                 <div className="">
                   <Container className=" dapp   Card ">
                     <div className="text-center ">
@@ -255,16 +276,16 @@ function DetailPage() {
                   </Container>
                 </div> */}
 
-                <div className="d-flex gap-1 justify-content-center">
-                  <div className=" dpad ">
-                    <ApplyJob job={jobID} />
-                    {/* <button className="dgabtn dheading mt-1 d-flex text-center justify-content-center">
+                                <div className="d-flex gap-1 justify-content-center">
+                                    <div className=" dpad ">
+                                        <ApplyJob job={jobID} />
+                                        {/* <button className="dgabtn dheading mt-1 d-flex text-center justify-content-center">
                       get job alert
                     </button> */}
-                  </div>
-                </div>
+                                    </div>
+                                </div>
 
-                {/* <Container className="mt-5">
+                                {/* <Container className="mt-5">
                   <h5>
                     Not quite what you are looking for? Try these similar
                     searches
@@ -278,13 +299,13 @@ function DetailPage() {
                     </Col>
                   </Row>
                 </Container> */}
-              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default DetailPage;
